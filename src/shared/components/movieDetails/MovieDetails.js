1bcc163de05ad04b0055c2fb9dfc6d37da1c32c9
Roman <gnatyuk.info@gmail.com>
2020-09-27 21:44:57 +0300

    Add new layout and styles for MovieDetails component

--- src/shared/components/movieDetails/MovieDetails.js
+++ src/shared/components/movieDetails/MovieDetails.js
@@ -5,45 +5,104 @@ import { withRouter } from 'react-router';
 // services
 import fetch from '../../../api/fetcher';
 
+// styles
+import './movieDetails.scss';
+
 class MovieDetails extends Component {
   state = {
     movie: null,
   };
 
-  async componentDidMount() {
+  componentDidMount() {
     const { movieID } = this.props.match.params;
 
-    await this.fetchMovie(movieID);
+    this.fetchMovie(movieID);
   }
 
   fetchMovie = async id => {
-    const data = await fetch.getMovieByID(id);
+    try {
+      const data = await fetch.getMovieByID(id);
 
-    this.setState({ movie: data || null });
+      this.setState({
+        movie: data || null,
+      });
+    } catch (error) {
+      console.error('MovieDetails fetch movie error: ', error);
+      throw new Error(error);
+    }
+  };
+
+  htmlLeftBlock = () => {
+    const { movie } = this.state;
+
+    return (
+      <>
+        <div className="details__poster">
+          <img
+            className="details__poster_img"
+            src={movie.Poster}
+            alt={movie.Title}
+          />
+        </div>
+
+        <div className="details__rating">
+          <span className="details__rating_title">IMDb Rating:</span>
+          <br />
+          <span className="details__rating_value">{movie.imdbRating} / </span>
+          <span className="details__rating_title">10</span>
+        </div>
+      </>
+    );
+  };
+
+  htmlRightBlock = () => {
+    const { movie } = this.state;
+
+    return (
+      <>
+        <div className="details__title">
+          <span className="details__title_title">{movie.Title}</span>
+        </div>
+
+        <div className="details__runtime">
+          <span className="details__key">Runtime: </span>
+          <span className="details__value">{movie.Runtime}</span>
+        </div>
+
+        <div className="details__genre">
+          <span className="details__key">Genre: </span>
+          <span className="details__value">{movie.Genre}</span>
+        </div>
+
+        <div className="details__year">
+          <span className="details__key">Year: </span>
+          <span className="details__value">{movie.Year}</span>
+        </div>
+
+        <div className="details__type">
+          <span className="details__key">Type: </span>
+          <span className="details__value">{movie.Type}</span>
+        </div>
+
+        <div className="details__plot">
+          <span className="details__plot_title">{movie.Plot}</span>
+        </div>
+      </>
+    );
   };
 
   render() {
     const { movie } = this.state;
 
     return (
-      <>
-        <h3>Movie details</h3>
-        {movie && (
-          <div style={{ display: 'flex' }}>
-            <img src={movie.Poster} alt={movie.Title} />
-            <div style={{ padding: '5rem' }}>
-              <p>{movie.Title}</p>
-              <p>{movie.Runtime}</p>
-              <p>{movie.Genre}</p>
-              <p>{movie.Year}</p>
-              <p>{movie.Type}</p>
-              <p>{movie.Plot}</p>
-              <p>{movie.imdbRating}</p>
-            </div>
-          </div>
-        )}
-        <br />
-      </>
+      <div className="details__wrapper">
+        <div className="details__leftblock">
+          {movie && this.htmlLeftBlock()}
+        </div>
+        <div className="details__rightblock">
+          {movie && this.htmlRightBlock()}
+        </div>
+      </div>
     );
   }
 }
