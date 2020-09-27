9764b8efcf2d7ea4a6d63b0dafd7905267176e88
Roman <gnatyuk.info@gmail.com>
2020-09-28 00:30:06 +0300

    Hotfix main title url path

--- src/shared/components/title/Title.js
+++ src/shared/components/title/Title.js
@@ -1,26 +1,17 @@
 // modules
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
-import { Link } from 'react-router-dom';
 
 // styles
 import './title.scss';
 
 class Title extends Component {
   render() {
-    const { pathname, search } = this.props.location;
-
     return (
       <h1 className="title">
-        <Link
-          className="title__text"
-          to={{
-            pathname: '/',
-            state: { pathLocal: pathname + search },
-          }}
-        >
+        <a href="/" className="title__text">
           Explore movies & series
-        </Link>
+        </a>
       </h1>
     );
   }
