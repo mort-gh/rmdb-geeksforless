7389075d48dffa7d7abb9dfd686132741737393d
Roman <gnatyuk.info@gmail.com>
2020-09-27 21:45:50 +0300

    Add new layout and styles for Title component

--- src/shared/components/title/Title.js
+++ src/shared/components/title/Title.js
@@ -3,19 +3,25 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
 
+// styles
+import './title.scss';
+
 class Title extends Component {
   render() {
     const { pathname, search } = this.props.location;
 
     return (
-      <Link
-        to={{
-          pathname: '/',
-          state: { pathLocal: pathname + search },
-        }}
-      >
-        <h1 style={{ color: 'white' }}>Explore movies & series</h1>
-      </Link>
+      <h1 className="title">
+        <Link
+          className="title__text"
+          to={{
+            pathname: '/',
+            state: { pathLocal: pathname + search },
+          }}
+        >
+          Explore movies & series
+        </Link>
+      </h1>
     );
   }
 }
