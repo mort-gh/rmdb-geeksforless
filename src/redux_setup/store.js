a38da75d5f71421f15a1d0d5aa639610ad4ef4cc
Roman <gnatyuk.info@gmail.com>
2020-09-28 16:51:10 +0300

    Create combineReducer and add action types

--- src/redux_setup/store.js
+++
@@ -1,6 +0,0 @@
-// export const initialState = {
-//   search: { searchQuery: '', currentPage: 1, movies: [], totalResults: 0 },
-//   movieDetails: { movie: null },
-//   slider: { slides: [], currentSlideIdx: 0, currentSlideData: null },
-//   trailer: { pathLocal: '' },
-// };
