d8f5180b74378b68c143280fcb73eb782b28fc3e
Roman <gnatyuk.info@gmail.com>
2020-09-28 16:56:30 +0300

    Add a reducer to the MovieDetails component

---
+++ src/shared/components/movieDetails/movieDetailsReducer.js
@@ -0,0 +1,34 @@
+import { initialState } from './movieDetailsStore';
+import {
+  FETCH_MOVIE_BY_ID_START,
+  FETCH_MOVIE_BY_ID_SUCCESS,
+  FETCH_MOVIE_BY_ID_ERROR,
+} from 'redux_setup/types';
+
+export function movieDetails(state = initialState, { type, payload }) {
+  switch (type) {
+    case FETCH_MOVIE_BY_ID_START:
+      return {
+        ...state,
+        spinner: true,
+      };
+
+    case FETCH_MOVIE_BY_ID_SUCCESS:
+      return {
+        ...state,
+        movie: payload,
+        spinner: false,
+        isLoaded: true,
+      };
+
+    case FETCH_MOVIE_BY_ID_ERROR:
+      return {
+        ...state,
+        spinner: false,
+        error: payload,
+      };
+
+    default:
+      return state;
+  }
+}
