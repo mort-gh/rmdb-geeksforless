22543fc8d65bf8805192fec1ba3a1fa14446b457
Roman <gnatyuk.info@gmail.com>
2020-09-30 11:40:59 +0300

    New action fn to fetch on scrolling

--- src/shared/components/search/searchActions.js
+++ src/shared/components/search/searchActions.js
@@ -4,7 +4,9 @@ import {
   FETCH_MOVIES_BY_QUERY_ERROR,
   FETCH_MOVIES_BY_QUERY_START,
   FETCH_MOVIES_BY_QUERY_SUCCESS,
+  FETCH_MOVIES_BY_QUERY_SCROLL,
   SAVE_URL_PARAMS,
+  TURN_OFF_SPINNER,
 } from '../../../redux_setup/types';
 
 const API_KEY = `8b47da7b`;
@@ -21,10 +23,34 @@ export function fetchMoviesByQuery(query, page) {
         data = await axios.get(`?apikey=${API_KEY}&s=${query}&page=${page}`);
       }
 
-      if (data && !data.data.Error) dispatch(fetchMoviesSuccess(data.data));
-      else dispatch(fetchMoviesError(data.data.Error));
+      if (data && !data.data.Error) {
+        dispatch(fetchMoviesSuccess(data.data));
+      } else {
+        dispatch(fetchMoviesError(data.data.Error));
+      }
 
       window.scrollTo({ top: 730, behavior: 'smooth' });
+    } catch (error) {
+      dispatch(fetchMoviesError(error));
+    }
+  };
+}
+
+export function fetchMoviesByScroll(query, page) {
+  return async dispatch => {
+    dispatch(fetchMoviesStart());
+    dispatch(saveURLparams(query, page));
+
+    try {
+      const data = await axios.get(
+        `?apikey=${API_KEY}&s=${query}&page=${page}`
+      );
+
+      if (data && !data.data.Error) {
+        dispatch(fetchMoviesScrollSuccess(data.data));
+      } else {
+        dispatch(turnOffSpinner());
+      }
     } catch (error) {
       dispatch(fetchMoviesError(error));
     }
@@ -39,7 +65,14 @@ export function fetchMoviesStart() {
 
 export function fetchMoviesSuccess(data) {
   return {
-    type: FETCH_MOVIES_BY_QUERY_SUCCESS,
+    type: FETCH_MOVIES_BY_QUERY_SUCCESS || FETCH_MOVIES_BY_QUERY_SCROLL,
+    payload: data,
+  };
+}
+
+export function fetchMoviesScrollSuccess(data) {
+  return {
+    type: FETCH_MOVIES_BY_QUERY_SCROLL,
     payload: data,
   };
 }
@@ -55,5 +88,11 @@ export function saveURLparams(query, page) {
   return {
     type: SAVE_URL_PARAMS,
     payload: { searchQuery: query, currentPage: page },
+  };
+}
+
+export function turnOffSpinner() {
+  return {
+    type: TURN_OFF_SPINNER,
   };
 }
