3361ccb97cf010b39c4d94315035028972385964
Roman <gnatyuk.info@gmail.com>
2020-10-07 13:25:26 +0300

    Remove test initialization file

--- src/__tests__/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Title from '../shared/components/title/Title.js';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Title />, div);
-});
